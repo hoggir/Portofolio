33a2e65de6ac1cde9deddd560fb9be41c1811dfd
Fadel Giras <fadelpopeye@gmail.com>
2022-07-29 19:09:31 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import Homev2 from "./pages/v2/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <Homev2 />
+      <Homev2 />
     </div>
   );
 }

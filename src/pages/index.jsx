71c14f095ae225dbe97b7a16434eafe4e0e023a1
Fadel Giras <fadelpopeye@gmail.com>
2022-07-29 22:12:38 +0700

    first-commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import V2Home from "./v2/Home";
 
 const Page = () => {
   return (
@@ -8,6 +9,7 @@ const Page = () => {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/v2" element={<V2Home />} />
         </Routes>
       </Router>
     </div>

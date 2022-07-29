5f75980baca52fe0118e6f75b0d4759b54a2a0d6
Fadel Giras <fadelpopeye@gmail.com>
2022-07-29 19:04:49 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import Homev2 from "./pages/v2/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/v2" element={<Homev2 />} />
-        </Routes>
-      </Router>
+        <Homev2 />
     </div>
   );
 }

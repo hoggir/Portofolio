e1f47627ac146c508bf501ccce86b8a051a84728
Fadel Giras <fadelpopeye@gmail.com>
2022-07-29 18:59:28 +0700

    first-commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const element = render(<App />);
-  const linkElement = el.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });

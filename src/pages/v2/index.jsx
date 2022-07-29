c25e79d359cc6f463114d8c836449c68ac327b06
Fadel Giras <fadelpopeye@gmail.com>
2022-07-29 19:22:42 +0700

    first-commit

--- src/pages/v2/index.jsx
+++ src/pages/v2/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import V2Home from "./Home";
 
 const Componentscon = () => {
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<V2Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 };

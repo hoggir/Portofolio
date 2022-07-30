192f378e41f032b26c33ed3bc40526c37edc148b
Fadel Giras <fadelpopeye@gmail.com>
2022-07-30 18:27:42 +0700

    add-skills

--- src/component/v2/Skills/index.jsx
+++ src/component/v2/Skills/index.jsx
@@ -9,7 +9,52 @@ export default function V2skills() {
           <h2>skills</h2>
         </div>
         <div className="v2skills-box">
-          <div className="v2skill-flex"></div>
+          <div className="v2skill-flex">
+            <div className="v2ppp">
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>html</h5>
+                </div>
+              </div>
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>css</h5>
+                </div>
+              </div>
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>javascript</h5>
+                </div>
+              </div>
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>rest apis</h5>
+                </div>
+              </div>
+            </div>
+            <div className="v2ppp">
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>node</h5>
+                </div>
+              </div>
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>react</h5>
+                </div>
+              </div>
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>mongodb</h5>
+                </div>
+              </div>
+              <div className="v2skill-box">
+                <div className="v2skill-item">
+                  <h5>responsive design</h5>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

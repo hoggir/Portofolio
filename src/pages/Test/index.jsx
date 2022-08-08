5bae4a31e2ce75eaca1318c7f700fde168c53bba
Fadel Giras <fadelpopeye@gmail.com>
2022-08-09 02:34:28 +0700

    add-test

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Rocket from "../../assets/rocket.png";
 import "./index.css";
 
 export default function TestPage() {
   return (
-    <div>
+    <div className="bg-port">
       <Container>
         <div className="test-con">
           <div className="test-con-left">
             <div className="test-con-left-con">
               <div className="test-con-left-con-100">
-                <div className="test-con-left-con-col">COL</div>
-                <div className="test-con-left-con-col">COL</div>
+                <div className="test-con-left-con-col">
+                  <h4>hi there! i'm</h4>
+                  <h1>Fadel Giras</h1>
+                  <p>
+                    I'm a web developer with a focus on the MERN stack, but
+                    still exploring other technologies and frameworks that catch
+                    my interest! if you're looking for a developer to add to
+                    your team, I'd love to hear from you!
+                  </p>
+                  <div className="test-con-left-action">
+                    <button className="test-btn-blur-bg">resume</button>
+                    <span>or</span>
+                    <button className="test-btn-blur-bg">send mail</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div className="test-con-right">
-            <h1>RIGHT</h1>
+            <div className="test-con-right-con">
+              <div className="test-con-right-con-imgcon">
+                <img className="test-con-right-con-img" src={Rocket} alt="" />
+              </div>
+            </div>
           </div>
         </div>
       </Container>
